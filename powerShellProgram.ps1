03f93487d1292a3c0dfd2150c2eef86023b5a32c
Mitsu-1216 <adieuer._.03.26@icloud.com>
2024-10-15 21:35:08 +0900

    ps1

--- powerShellProgram.ps1
+++ powerShellProgram.ps1
@@ -1,14 +1,14 @@
-# 置換対象のディレクトリを指定
-$directory = "C:\path\to\your\directory"
+# 使用者にディレクトリパスを入力してもらう
+$directory = Read-Host "ファイル名を置換する対象のディレクトリのパスを入力してください"
 
-# 置換前と置換後の文字列を指定
-$oldText = "置換前の文字列"
-$newText = "置換後の文字列"
+# 使用者に置換前と置換後の文字列を入力してもらう
+$oldText = Read-Host "置換したい文字列を入力してください"
+$newText = Read-Host "新しい文字列を入力してください"
 
 # ディレクトリ内のファイルを取得し、ファイル名を置換
 Get-ChildItem -Path $directory -File | ForEach-Object {
     $oldName = $_.Name
-    $newName = $oldName -replace $oldText, $newText
+    $newName = $oldName -replace [regex]::Escape($oldText), $newText
     if ($oldName -ne $newName) {
         Rename-Item -Path $_.FullName -NewName $newName
         Write-Host "ファイル名を変更しました: '$oldName' → '$newName'"
